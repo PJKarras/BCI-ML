734d015b85144886b73e37db779c77f5d8ab955b
PJKArras <pjk18b@my.fsu.edu>
2021-02-18 13:49:54 -0500

    Test Runs with Split Data
    Saved data from test runs with sky, middle, and bottom of image. Required changes in network.

--- Graph_Making.R
+++ Graph_Making.R
@@ -29,14 +29,31 @@ text(11.2,34,"0.8905117", cex = 2, col="dark red", font=2)
 
 # NEW STUFF PAR ANAYLSIS
 setwd("C:/Users/Peter/Desktop/Thermo_R_Code/FinalThermo/new/BCI-ML/Images and Plots/PAR Data")
-file_name <- "merged_PAR_Analysis_test_data_DAYTIME_13-446994955314361.csv"
+file_name <- "merged2_PAR_Analysis_test_data_DAYTIME_13-446994955314361.csv"
 
 data <- read.csv(file = file_name)
 data$PARDIFF
-plot(data$PARDIFF, data$gppdiff, main="PAR Diff vs GPP Error",cex.main=1.95,cex=0.8,cex.axis=2,cex.lab=1.7, xlab="PAR Diff", ylab="GPP Error")
+plot(t_score, data$gppdiff, main="PAR T-score vs GPP Error",cex.main=1.95,cex=0.8,cex.axis=2,cex.lab=1.7, xlab="PAR T-Score", ylab="GPP Error")
 axis(1,cex.axis=2)
 abline(a=0,b=1, col="red")
-cor(data$PARDIFF, data$gppdiff)**2
+cor(t_score, data$gppdiff)**2
+text(7.8,18,"R^2:", cex = 2, col="dark red")
+text(9.5,18,"0.00148681", cex = 2, col="dark red", font=2)
+
+# Relationship betyween GPP and PAR?
+# NEW STUFF PAR ANAYLSIS
+setwd("C:/Users/Peter/Desktop/Thermo_R_Code/FinalThermo/new2/")
+file_name <- "PAR_full_analysistest_data.csv"
+
+
+data <- read.csv(file = file_name)
+t_score <- data$PARDIFF / (data$std/(sqrt(data$count)))
+boxplot(data$PARDIFF,main="Distribution of PARDIFF Values in Dataset",xlab="PARDIFF",horizontal = TRUE)
+hist(data$PARDIFF, main="Distribution of PARDIFF Values for Full Dataset", xlab="PARDIFF", freq=TRUE)
+plot(data$PAR, data$GPP, main="PAR vs GPP",cex.main=1.95,cex=0.8,cex.axis=2,cex.lab=1.7, xlab="PAR", ylab="GPP")
+axis(1,cex.axis=2)
+abline(a=0,b=1, col="red")
+cor(data$PAR, data$GPP)**2
 text(1020,18,"R^2:", cex = 2, col="dark red") 
 text(1210,18,"0.06860969", cex = 2, col="dark red", font=2)
 
